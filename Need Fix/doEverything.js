14327a09809906a0e99f3102ec82481c585f0f08
SethReis <795091@seq.org>
2017-12-04 13:01:37 -0800

    Add files via upload

---
+++ Need Fix/doEverything.js
@@ -0,0 +1,97 @@
+window.onload = init; // Wait for the page to load before we begin animation
+var canvas;
+var ctx;// This is a better name for a global variable
+var k = 0;
+var flock;
+var mouseX, mouseY;
+var mouseLoc = new JSVector(0, 0);
+var countTimes = 0;
+
+function init(){
+  //get the canvas
+  canvas = document.getElementById('cnv');
+  // Set the dimensions of the canvas
+  flock = new Flock();
+  canvas.style.border = 'solid black 0px';
+  //var color = Math.floor((Math.random()*2)+1);
+  //canvas.style.backgroundColor = '';
+  canvas.style.backgroundColor = 'rgba(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.random() + ')';
+  // get the context
+  ctx = canvas.getContext('2d'); // This is the context
+  for (var i = 0; i < 3; i++){
+    flock.addBoid(new Boid(i, new JSVector(Math.random()*canvas.width, Math.random()*canvas.height)));
+  }
+  canvas.addEventListener('mousemove', function(event){
+    mouseX = event.offsetX;     // Get the mouse coordinate
+    mouseY = event.offsetY;
+    countTimes++;
+    if (countTimes >= 5 && event.buttons == 1){
+      flock.addBoid(new Boid(i, new JSVector(mouseX, mouseY)));
+      countTimes = 0;
+    }
+  }
+  , false);
+  mover = new Mover();
+  orb = new Orb();
+  snake = new Snake();
+  animate(); // Call to your animate function
+}
+// To do::
+//  1. Declare and init variables x, y, dx, dy, radius;
+function animate(){
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  k++;
+  // Looping through backwards to delete
+  for (var i = flock.boids.length-1; i >= 0; i--) {
+    if (k === -150){
+      flock.addBoid(new Boid(i, new JSVector(canvas.length/2, canvas.width/2)));
+      //flock.addBoid(new Boid(i, new JSVector(snake.segs[snake.segs.length-1].x, snake.segs[snake.segs.length-1].y)));
+      // for (var i = 0; i < flock.boids.length; i++){
+      //   flock.addBoid(new Boid(i, new JSVector(flock.boids[i].loc.x, flock.boids[i].loc.y)));
+      // }
+      k = 0;
+    }
+    var boid = flock.boids[i];
+    boid.update();
+    boid.render();
+    if (boid.isDead()) {
+      //remove the boidicle
+      flock.boids.splice(i, 1);
+    }
+    var distance = mover.loc.distance(boid.loc)
+    if (distance <= 500){
+      var f = JSVector.subGetNew(boid.loc, mover.loc);
+      f.normalize();
+      f.mult(.3);
+      mover.applyForce(f);
+    }
+  }
+  mover.update();
+  mover.render();
+  for(var j = 0; j < flock.boids.length; j++){
+    var boid= flock.boids[j];
+    var distance = boid.loc.distance(mover.loc)
+    if (distance <= 200){
+      var f = JSVector.subGetNew(boid.loc, JSVector.addGetNew(mover.loc, orb.loc));
+      f.normalize();
+      f.mult(.3);
+      boid.applyForce(f);
+    }
+  }
+  orb.update();
+  orb.render();
+  snake.update();
+  snake.render();
+  for(var j = flock.boids.length-1; j >= 0; j--){
+    var boid = flock.boids[j];
+    var distance = boid.loc.distance(JSVector.addGetNew(mover.loc, orb.loc));
+    if (distance <= snake.rad + 2){
+      flock.boids.splice(j, 1);
+      for (var i = 0; i < 5; i++){
+        snake.segs.push(new JSVector(0, 0));
+        snake.rad += 0.1;
+      }
+    }
+  }
+}
---
+++ Need Fix/mover.js
@@ -0,0 +1,41 @@
+function Mover(){
+  this.rad = 10;
+  this.loc = new JSVector((Math.random()*canvas.width-(2*this.rad))+this.rad, (Math.random()*canvas.height-(2*this.rad))+this.rad);
+  this.vel = new JSVector(Math.random()*10, Math.random()*10);
+  this.acc = new JSVector(0, 0);
+  this.c = 'rgba(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.random() + ')';
+}
+
+Mover.prototype.update = function(){
+  this.vel.add(this.acc);
+  this.vel.limit(4);
+  this.loc.add(this.vel);
+  this.acc.mult(0);
+  if(this.loc.x + this.rad >= canvas.width || this.loc.x - this.rad <= 0){
+    //this.loc.x = canvas.width-this.loc.x;
+    this.vel.x = -(this.vel.x);
+  }
+  if(this.loc.y + this.rad >= canvas.height || this.loc.y - this.rad <= 0){
+    //this.loc.y= canvas.height-this.loc.y;
+    this.vel.y = -(this.vel.y);
+  }
+}
+
+Mover.prototype.render = function(){
+  ctx.beginPath();
+  ctx.arc(this.loc.x,this.loc.y,this.rad,0,2*Math.PI);
+  ctx.fillStyle = this.c;
+  ctx.fill();
+  ctx.strokeStyle = this.c;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.moveTo(canvas.width, 0);
+  ctx.lineTo(0, canvas.height);
+  ctx.stroke();
+}
+
+Mover.prototype.applyForce = function(f){
+  this.acc.add(f);
+}
